b51b16cc048c07747a0ce3184a64cd6ef89f5d8b
Ashari Juang <asharijuang@me.com>
2015-03-22 11:01:47 +0700

    add validation on comment and post, create nested roesource for comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+	# Membuat relasi dengan table comment dan jika dihapus post maka semua comment yang terkait akan terhapus
+	# Jenis relasi 1 post memiliki banyak comment (has_many)
+	has_many :comments, dependent: :destroy
+
+	validates :title, :body, presence: true
 end
